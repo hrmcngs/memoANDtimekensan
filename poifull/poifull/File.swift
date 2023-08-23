e9c0cc702384d3d69bc8f1e9d45ce447d772d270
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2023-08-23 14:05:32 +0900

    memocommit

---
+++ poifull/poifull/File.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+class File: UIViewController {
+
+
+    @IBAction func url(_ sender: Any) {
+    }
+    
+    let url = URL(string:"https://www.instagram.com/0526ngs/")
+    
+  
+    }
+
--- poifull/poifull/ViewController.swift
+++ poifull/poifull/ViewController.swift
@@ -1,25 +1,28 @@
-//
-//  ViewController.swift
-//  poifull
-//
-//  Created by Hiromichi Nagase on 2023/01/28.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var a: UITextView!
+class ViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var textView: UITextView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let name =
-            UserDefaults.standard.string(forKey: "name") {
-            a.text = name
-        }}
-    @IBAction func handleTextChange() {  
-        let text = a.text
-        print(a.text ?? "(null)")
-        UserDefaults.standard.setValue(text, forKey: "name")
+        textView.delegate = self
+        loadSavedText()
     }
-
+    
+    func loadSavedText() {
+        if let savedText = UserDefaults.standard.string(forKey: "savedText") {
+            textView.text = savedText
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if let newText = textView.text {
+            UserDefaults.standard.set(newText, forKey: "savedText")
+        }
+    }
 }
+
+
